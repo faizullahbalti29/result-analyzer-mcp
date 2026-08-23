bbd6e3a46c4662c7c83c5fff8a49c19c0f7a99a9
faizullah <faizullahbalti29@gmail.com>
2026-08-23 15:17:11 +0500

    cleared project

--- src/types/student.ts
+++
@@ -1,9 +0,0 @@
-export type Student = {
-    roll_no: string;
-    name: string;
-    status: string | null;
-    marks: number | null;
-    grade: string | null;
-    remarks: string | null;
-    institution: string | null;
-};
--- src/utils/escape-regex.ts
+++
@@ -1,3 +0,0 @@
-export function escapeRegex(value: string): string {
-    return value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
